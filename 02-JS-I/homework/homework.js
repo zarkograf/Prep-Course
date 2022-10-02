1b218abfcf393b61f467f0693aa09f9c36267157
julian munar <zarkograf623@gmail.com>
2022-10-02 17:57:20 -0500

    frist commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -147,18 +147,23 @@ function elevarAlCubo(num) {
 function elevar( num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código
+  return Math.pow(num, exponent);
+
+
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+
+  return Math.round( num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
   
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
